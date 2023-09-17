1eee834678e3ca0c36999d21f892ec6f275f41f3
saradonnelly97 <sara.donnelly8@gmail.com>
2023-09-17 15:30:53 -0400

    Adding plants view with mock data

--- controllers/plant.js
+++ controllers/plant.js
@@ -1,13 +1,30 @@
 const router = require('express').Router()
 
-router.get('/', async (req, res) => {
-    const plants = await Plant.find().populate('plants')
-    res.json(plants)
+// router.get('/', async (req, res) => {
+//     const plants = await Plant.find().populate('plants')
+//     res.json(plants)
+// })
+
+//GET Plants
+router.get('/', (req, res) => {
+    //Plant Mock Data
+    let plants = [{
+        name: 'Tomato',
+        light: '☀️☀️☀️☀️☀️',
+        water: '💧💧💧💧💧',
+        season: 'Summer',
+        pic: 'https://images.unsplash.com/photo-1596199050105-6d5d32222916?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
+    }, {
+        name: 'Strawberry',
+        light: '☀️☀️☀️☀️☀️',
+        water: '💧💧💧💧💧', 
+        season: 'Spring',
+        pic: 'https://images.unsplash.com/photo-1582757793483-e407049be006?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
+    }]
+    res.render('plants/index', { plants })
 })
 
-router.get('/', (req, res) => {
-    res.send('GET /plants. This is where the index is at')
-})
+  
 
 // router.get('/')
 
--- server.js
+++ server.js
@@ -27,9 +27,6 @@ app.get ('*', (req, res) => {
 })
 
 
-
-
-
 // DB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
