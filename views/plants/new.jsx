e008eb1e8ddf63de7795360c87a930d7ce8c0b90
saradonnelly97 <sara.donnelly8@gmail.com>
2023-09-17 15:38:32 -0400

    Adding in view stubs

---
+++ views/plants/new.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_form () {
+    return (
+        <Def>
+            <main>
+                <h1>Add a New Plant</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = new_plant
