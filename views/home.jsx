4beb970c957a9245cf0b7c3b35f291ded26bad89
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-09-19 21:29:37 -0400

    css + redwood

--- views/home.jsx
+++ views/home.jsx
@@ -14,18 +14,18 @@ function home () {
         </div>
         <div className="philosophy-container">
           <div className="philosophy">
-            <h2><i>Tell It How It Is</i></h2>
-            <p>And don't be scared!</p>
-            <p>Rest-RANT is a safe space where you can tell the world how you would rate other restaurants in the United States. Add a little blurb about the food and your overall experience there while you're at it! We're all ears! (Unlike your closest family) Was it the BEST Chinese food you've ever had? Did your server spit in your food but it actually added flavor?? Whatever happened, however it tasted, WE NEED TO KNOW.</p>
+            <h2><i>HarvestGarden</i></h2>
+            <p>Growing for a new tomorrow.</p>
+            <p>HarvestGarden is your home for finding out how to grow and cultivate plants in your collection and the great outdoors.</p>
             <a href="/places">
-            <button className="btn btn-danger btn-lg">RANT ABOUT IT BABY!!</button>
+            <button className="btn btn-danger btn-lg">Your thoughts</button>
           </a>
           </div>
         </div>
 
         <div className="articles">
           <div className="articleOne">
-            <img className="articleOneImage" src="/images/annieSpratt.jpeg" alt="Article One" />
+            <img className="articleOneImage" src="/images/redwood.jpeg" alt="Article One" />
             <div className="container">
               <h4>inserting some words</h4>
               <p>inserting some words here because reasons...</p>
@@ -33,7 +33,7 @@ function home () {
           </div>
 
           <div className="articleTwo">
-            <img className="articleTwoImage" src="/images/annieSpratt2.jpeg" alt="Article Two" />
+            <img className="articleTwoImage" src="/images/redwood.jpeg" alt="Article Two" />
             <div className="container">
               <h4>inserting some words</h4>
               <p>inserting some words here because reasons...</p>
