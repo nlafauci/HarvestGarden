de6aa3840bc1b6ee5545908a08b75c6c0ccf97b2
saradonnelly97 <sara.donnelly8@gmail.com>
2023-09-20 15:48:29 -0400

    document formatting

--- views/home.jsx
+++ views/home.jsx
@@ -1,12 +1,12 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
-    return (
-        <Def>
-            <main>
-            <div>
-                <img src="https://images.unsplash.com/photo-1584479898061-15742e14f50d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" className="landing" alt="Photo of a sprouting plant" />
+function home() {
+  return (
+    <Def>
+      <main>
+        <div>
+          <img src="https://images.unsplash.com/photo-1584479898061-15742e14f50d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" className="landing" alt="Photo of a sprouting plant" />
           <div id="subtitle">
             Photo by <a href="https://unsplash.com/@jeremybishop">Jeremy Bishop</a> on <a href="https://unsplash.com/photos/vGjGvtSfys4">Unsplash</a>
           </div>
@@ -17,11 +17,11 @@ function home () {
             <p>Growing for a new tomorrow.</p>
             <p>HarvestGarden is your home for finding out how to grow and cultivate plants in your collection and the great outdoors.</p>
             <a href="/plants">
-            <button className="btn btn-danger btn-lg">Learn Now!</button>
-          </a>
+              <button className="btn btn-danger btn-lg">Learn Now!</button>
+            </a>
           </div>
         </div>
-        
+
         <div className="articles">
           <div className="articleOne">
             <img className="articleImage" src="https://plus.unsplash.com/premium_photo-1687987358293-13b376873356?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Article One" />
@@ -38,10 +38,10 @@ function home () {
               <p>This site includes plant species and subspecies, water and light specifications of plants, and whether these plants are best suited for indoors and outdoors. Viewers can comment and submit queries about their specific methods of growing these plants, which are moderated by the site team.</p>
             </div>
           </div>
-          </div>
-          </main>
-        </Def>
-    )
+        </div>
+      </main>
+    </Def>
+  )
 }
 
 module.exports = home
