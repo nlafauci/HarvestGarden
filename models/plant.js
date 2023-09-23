1f37db0e023ab7125151b28f48764f25d15b90f3
saradonnelly97 <sara.donnelly8@gmail.com>
2023-09-23 13:19:36 -0400

    Added in description to model; Adjusted needsLight and needsWater

--- models/plant.js
+++ models/plant.js
@@ -22,6 +22,7 @@ const plantSchema = new mongoose.Schema({
     type: String,
     default: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
   },
+  //Added in the description to model
   description: {
     type: String, 
     required: true, 
