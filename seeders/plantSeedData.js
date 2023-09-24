5340b280a889235521be73d5d8489247b3251b82
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-09-24 14:34:12 -0400

    more seed data

--- seeders/plantSeedData.js
+++ seeders/plantSeedData.js
@@ -22,24 +22,27 @@ db.Plant.deleteMany()
         needsWater: 'Water Daily',
         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQofNzQ5NggTZ3v4oyTZmrDpfS9xgr2otUgl_poUCpv5nV2zg-0uwb0BEZttoeQPtMDqc0&usqp=CAU',
         description:'You/ve probably seen me on Phineas & Fern'
-    }, {
+    },{
         name: 'Carrot',
         isIndoor: false,
-        needsLight: '8',
-        needsWater: '4',
-        image: 'https://images.unsplash.com/photo-1590868309235-ea34bed7bd7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1827&q=80'
+        needsLight: 'Full Sunlight',
+        needsWater: 'Water Weekly',
+        image: 'https://images.unsplash.com/photo-1582515073490-39981397c445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80',
+        description: 'Little orange dudes full of incredible vitamins and minerals. Knows how to make a rabbit happy.'
     }, {
         name: 'Orchids',
         isIndoor: true,
-        needsLight: '6',
-        needsWater: '3',
+        needsLight: 'Indirect Sunlight',
+        needsWater: 'Water Weekly',
         image: 'https://www.sunset.com/wp-content/uploads/orchids-pc-dmitriy-sidor-getty-1200x600.jpg',
+        description: 'Orchid care is not difficult; just different!'
     }, {
         name: 'Poison Ivy',
         isIndoor: false,
-        needsLight: '8',
-        needsWater: '6',
-        image: 'https://en.wikipedia.org/wiki/Toxicodendron_radicans#/media/File:2014-10-29_13_43_39_Poison_Ivy_foliage_during_autumn_leaf_coloration_in_Ewing,_New_Jersey.JPG'
+        needsLight: 'Full Sunlight',
+        needsWater: 'Water Every Other Week',
+        image: 'https://www.verywellhealth.com/thmb/-ydqcKSn-B44ihm5fWzkNQjSwGc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-951410190-b5e263bc76f44d05a02979586ae7cc8a.jpg',
+        description: 'Harbringers of itchiness I would not wish upon my worst enemies. Or would I...?'
     }])
     .then(() => {
         console.log('Success!')
@@ -50,3 +53,5 @@ db.Plant.deleteMany()
         process.exit()
     })
 })
+
+
