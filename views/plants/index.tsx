4ba164f74493c399c5953fab09206f09a92862d8
saradonnelly97 <sara.donnelly8@gmail.com>
2023-11-15 13:16:26 -0500

    ed in ne sh views typescript migration attempted

--- views/plants/index.tsx
+++ views/plants/index.tsx
@@ -1,26 +1,36 @@
-const React = require('react')
-const Def = require('../default')
+import React from 'react';
+import Def from '../default';
 
-function index (data) {
-    let plantsFormatted = data.plants.map((plant) => {
-      return (
-        <div key={plant.id} className="col-md-4">
-          <h2>
-            <a href={`/plants/${plant.id}`}>{plant.name}</a>
-          </h2>
-          <img className="inGrid" src={plant.image} alt={plant.name}/>
-        </div>  
-      )
-    })
-    return (
-      <Def>
-          <main>
-              <h1>PLANTS INDEX PAGE</h1>
-              <div className="row">{ plantsFormatted }</div>
-          </main>
-      </Def>
-  )
-  }
-  
+interface Plant {
+  id: string;
+  name: string;
+  image: string;
+}
 
-module.exports = index
+interface IndexProps {
+  data: {
+    plants: Plant[];
+  };
+}
+
+function Index({ data }: IndexProps): JSX.Element {
+  const plantsFormatted = data.plants.map((plant) => (
+    <div key={plant.id} className="col-md-4">
+      <h2>
+        <a href={`/plants/${plant.id}`}>{plant.name}</a>
+      </h2>
+      <img className="inGrid" src={plant.image} alt={plant.name} />
+    </div>
+  ));
+
+  return (
+    <Def>
+      <main>
+        <h1>PLANTS INDEX PAGE</h1>
+        <div className="row">{plantsFormatted}</div>
+      </main>
+    </Def>
+  );
+}
+
+export default Index;
